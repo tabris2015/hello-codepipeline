7d79a01ad18a9424b66211369a8f48e490abf085
Jose Laruta <eduardo.laruta@gmail.com>
2022-06-01 22:19:20 -0400

    fix format

--- app/create_table.py
+++ app/create_table.py
@@ -1,26 +1,22 @@
-
 def create_users_table(dynamodb, table_name):
     # Table defination
     table = dynamodb.create_table(
         TableName=table_name,
         KeySchema=[
-            {
-                "AttributeName": "username",
-                "KeyType": "HASH"  # Partition key
-            },
+            {"AttributeName": "username", "KeyType": "HASH"},  # Partition key
         ],
         AttributeDefinitions=[
             {
                 "AttributeName": "username",
                 # AttributeType defines the data type. "S" is string type and "N" is number type
-                "AttributeType": "S"
+                "AttributeType": "S",
             },
         ],
         ProvisionedThroughput={
             # ReadCapacityUnits set to 10 strongly consistent reads per second
             "ReadCapacityUnits": 10,
-            "WriteCapacityUnits": 10  # WriteCapacityUnits set to 10 writes per second
-        }
+            "WriteCapacityUnits": 10,  # WriteCapacityUnits set to 10 writes per second
+        },
     )
     return table
 
